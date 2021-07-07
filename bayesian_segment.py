6aea162b443c97a3702a3437c380254f5d523537
Yingru <arantilwu@gmail.com>
2021-07-06 23:26:10 -0400

    updates

--- bayesian_segment.py
+++ bayesian_segment.py
@@ -1,35 +1,41 @@
-fn = ".csv"
+file_name = "C:/Users/Yingru/Documents/GitHub/bisulfite/example/dsid_C_1461_0.csv"
 
 import math
 import numpy as np
-from scipy.special import logsumexp
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.special import logsumexp
 
 
 class Dsid:
-    def __init__(self, filename, sc = 25):
+    '''
+    This class is a bisulfite sequence dataset
+    '''
+
+    def __init__(self, filename, scale = 25):
+        '''
+        filename: the bisulfite sequence dataset .csv file name
+        scale: scale makes all datasets have the same scaled totFeq, thus same variance, 25 good in practice.
+        '''
         self.filename = filename;
         self.reads = pd.read_csv(self.filename).set_index("id");
         self.index = [int(x[2:]) for x in self.reads.columns[:-1]];
         self.n = len(self.index);
         self.totSeq = np.zeros(self.n);
         self.totFeq = 0;
-        for read in self.reads.iterrows():
-            self.totFeq += read[1][-1];
-            self.totSeq += read[1][:-1] * read[1][-1];
-        self.N = self.totFeq;
-        self.totSeq = self.totSeq / self.totFeq * sc;
-        self.totFeq = sc;
+        for _, row in self.reads.iterrows():
+            self.totFeq += row[-1];
+            self.totSeq += row[:-1] * row[-1];
+        self.totSeq = self.totSeq / self.totFeq * scale;
+        self.totFeq = scale;
         self.a0 = self.totFeq;
-        self.mu0 = sum(self.totSeq) / (self.n * self.totFeq);
+        self.mu0 = np.sum(self.totSeq) / (self.n * self.totFeq);
         self.alp0 = self.mu0 * self.a0;
         self.lam0 = 0;
         self.p = [];
         self.Yij = np.zeros((self.n, self.n));
         for i in range(self.n):
             self.Yij[i, i:] = self.alp0 + np.cumsum(self.totSeq[i:]);
-        
         # filter and backward filter
         self.Pit = np.zeros((self.n, self.n));
         self.Pits = np.zeros((self.n, self.n));
@@ -40,20 +46,17 @@ class Dsid:
         self.I = np.zeros(self.n);
         self.Thetat = np.zeros(self.n);
         return;
-    
-    
+
     # beta-binomial
     def pipi(self, a, b, c, d):
         if a == -1 and b == -1:
             return math.lgamma(self.Yij[c][d]) + math.lgamma(self.a0 + (d-c+1)*self.totFeq - self.Yij[c][d]) + math.lgamma(self.a0) - math.lgamma(self.a0 + (d-c+1)*self.totFeq) - math.lgamma(self.alp0) - math.lgamma(self.a0 - self.alp0);
         else:
             return math.lgamma(self.Yij[c][d]) + math.lgamma(self.a0 + (d-c+1)*self.totFeq - self.Yij[c][d]) + math.lgamma(self.a0 + (b-a+1)*self.totFeq) - math.lgamma(self.a0 + (d-c+1)*self.totFeq) - math.lgamma(self.Yij[a][b]) - math.lgamma(self.a0 + (b-a+1)*self.totFeq - self.Yij[a][b]);
-    
-    
+
     def pipipipi(self, a, b, c, d, e, f):
         return self.pipi(a, b, e, f) - self.pipi(-1, -1, c, d);
-    
-    
+
     def calculatePQ(self):
         for t in range(self.n):
             self.Pits[t, t] = np.log(self.p[t]) + self.pipi(-1, -1, t, t);
@@ -62,7 +65,7 @@ class Dsid:
             lse = logsumexp(self.Pits[t, 0 : t + 1]);
             for i in range(t, -1, -1):
                 self.Pit[t, i] = self.Pits[t, i] - lse;
-        
+
         for t in range(self.n - 2, -1, -1):
             self.Qjts[t + 1, t + 1] = np.log(self.p[t + 2]) + self.pipi(-1, -1, t + 1, t + 1);
             for j in range(t + 2, self.n):
@@ -71,8 +74,7 @@ class Dsid:
             for j in range(t + 1, self.n):
                 self.Qjt[t + 1, j] = np.log(1 - self.p[t + 1]) + self.Qjts[t + 1, j] - lse;
         return;
-    
-    
+
     def calculateBeta(self):
         for t in range(self.n):
             for i in range(t + 1):
@@ -82,23 +84,20 @@ class Dsid:
                     self.Bijts[t, i, j] = self.Pit[t, i] + self.Qjt[t + 1, j] + self.pipipipi(i, t, t + 1, j, i, j);
             self.Bs[t] = logsumexp(self.Bijts[t, 0 : t + 1, t : self.n].flatten());
         return;
-    
-    
+
     def calculateIt(self):
         self.I[0] = 1;
         for t in range(1, self.n - 1):
             self.I[t + 1] = self.p[t + 1] / np.exp(self.Bs[t]) if self.Bs[t] < 500 else 0;
         return;
-    
-    
+
     def calculateTheta(self):
         for t in range(self.n):
             for i in range(t + 1):
                 for j in range(t, self.n):
                     self.Thetat[t] += np.exp(self.Bijts[t, i, j] - self.Bs[t]) * self.Yij[i, j] / (self.a0 + (j - i + 1) * self.totFeq);
         return;
-    
-    
+
     def choose(self):
         candidate = (0.0, -np.inf);
         lams = -np.log(np.linspace(0.95, 0.9975, 20));
@@ -117,29 +116,29 @@ class Dsid:
             self.Pits = np.zeros((self.n, self.n));
             self.Qjt = np.zeros((self.n, self.n));
             self.Qjts = np.zeros((self.n, self.n));
-        
+
         self.lam0 = candidate[0];
         self.p = [1] + [1 - np.e ** (-self.lam0 * (self.index[x] - self.index[x - 1])) for x in range(1, len(self.index))] + [1];
         return;
-    
-    
+
     def write(self):
-        res = pd.DataFrame(np.append(self.I, 0).reshape((1, len(self.index)+1)), columns = self.reads.columns, index = ["prob of a change"]);
-        res2 = pd.DataFrame(np.append(self.Thetat, 0).reshape((1, len(self.index)+1)), columns = self.reads.columns, index = ["prob of response"]);
-        self.reads = pd.concat([res, res2, self.reads]);
-        self.reads.to_csv(self.filename[:-4] + "_res.csv");
+        res = pd.DataFrame(np.append(self.I, 0).reshape((1, len(self.index)+1)), columns = self.reads.columns, index = ["prob of change"]);
+        res1 = pd.DataFrame(np.append(self.Thetat, 0).reshape((1, len(self.index)+1)), columns = self.reads.columns, index = ["prob of response"]);
+        self.reads = pd.concat([res, res1, self.reads]);
+        self.reads.to_csv(self.filename[:-4] + "_result.csv");
         return;
 
 
 if __name__ == "__main__":
-    sample = Dsid(fn);
+    sample = Dsid(file_name);
     sample.choose();
     sample.calculatePQ();
     sample.calculateBeta();
     sample.calculateIt();
     sample.calculateTheta();
     sample.write();
-    
+
+    # output posterior estimations
     fig = plt.figure(figsize = (32, 6));
     a = sample.totSeq / sample.totFeq;
     b = np.array([math.sqrt(aa * (1 - aa)) / 2 for aa in a]);
@@ -149,7 +148,8 @@ if __name__ == "__main__":
     plt.xlabel("bp locations", fontsize = 20);
     plt.ylabel("frequencies of mutation", fontsize = 20);
     plt.show();
-    
+
+    # output patch size
     t = 0;
     peak = sample.totSeq.max();
     while sample.totSeq[t] != peak:
@@ -168,7 +168,7 @@ if __name__ == "__main__":
         prob[i] /= aa;
     for i, j in zip(patch_size, prob):
         mean += i * j;
-    fig = plt.figure(figsize=(10, 10));
+    fig = plt.figure(figsize = (10, 10));
     plt.bar(patch_size, prob, width = 0.5);
     plt.xticks(list(range(3, 23)), fontsize = 12);
     plt.yticks(fontsize = 12);
--- clustering.py
+++ clustering.py
@@ -1,24 +1,35 @@
-fn = "C:/Users/Yingru/Source/Repos/bisf/data/dsid_C_1461.csv"
-cn = 7
-
+file_name = "C:/Users/Yingru/Documents/GitHub/bisulfite/example/dsid_C_1461.csv"
+cluster_num = 7
 
 import pandas as pd
 from sklearn.cluster import AgglomerativeClustering
 
 
 def hamming(seq1, seq2):
+    '''
+    calculate hamming distance between two sequences
+    '''
     return sum(s1 != s2 for s1, s2 in zip(seq1, seq2));
 
 
-def divide(filename, c):
+def divide(filename, clusters):
+    '''
+    divide filename into clusters groups and store them separately
+    '''
     reads = pd.read_csv(filename).set_index("id");
-    seqs = [list(line[1][:-1]) for line in reads.iterrows()];
+    seqs = [list(row[:-1]) for _, row in reads.iterrows()];
     sMatrix = [[hamming(seq1, seq2) for seq2 in seqs] for seq1 in seqs];
-    labs = AgglomerativeClustering(n_clusters = c, affinity = "precomputed", linkage = "complete").fit(sMatrix).labels_;
-    for i in range(c):
-        reads.iloc[labs == i].to_csv(filename[:-4] + "_" + str(i) + ".csv");
+    labs = AgglomerativeClustering(
+        n_clusters = clusters,
+        affinity = "precomputed",
+        linkage = "complete",
+        ).fit(sMatrix).labels_;
+
+    for i in range(clusters):
+        reads.loc[labs == i].to_csv(filename[:-4] + '_' + str(i) + ".csv");
+
     return;
 
 
 if __name__ == "__main__":
-    divide(fn, cn);
+    divide(file_name, cluster_num);
